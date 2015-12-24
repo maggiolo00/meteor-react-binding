d1433d59590015c191402f23686db800c9dfdfc0
maggiolo00 <maggiolo00@gmail.com>
2015-12-24 18:49:01 +0100

    docs updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# meteor-react-binding
+# Meteor React Binding
+
+Exposes a globally available `BindToMixin` object from [https://github.com/rsamec/react-binding](react-binding).
+
+
+## Installation
+
+    meteor add wolf4ood:react-binding
+
+
--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'wolf4ood:react-binding',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Just a wrapper for react-binding.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/maggiolo00/meteor-react-binding',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
